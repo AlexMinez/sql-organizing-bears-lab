281559bdf59c80c7a133cf2d515cc68a93df83b7
AlexMinez <alex_minihan@outlook.com>
2022-06-30 15:49:07 +1000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+
+
+INSERT INTO bears VALUES (1,"Mr. Chocolate",20, "M", "blue","idk","1");
+INSERT INTO bears VALUES (2,"Rowdy", 20,"M", "blue","idk","1");
+INSERT INTO bears VALUES (3,"Tabitha", 20,"M", "blue","idk","1");
+INSERT INTO bears VALUES (4,"Sergeant Brown",20, "M", "blue","idk","1");
+INSERT INTO bears VALUES (5,"Melissa",20, "M","blue","idk","1");
+INSERT INTO bears VALUES (6,"Grinch", 20,"M", "blue","idk","1");
+INSERT INTO bears VALUES (7,"Wendy", 20,"M", "blue","idk","1");
+INSERT INTO bears VALUES (8,null, 20,"M", "blue","idk","1");
+
+
